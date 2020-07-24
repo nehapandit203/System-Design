4f0a45b755a9d868ca1da0493d7136be79df4b32
neha_pandit <neha.pandit@spglobal.com>
2020-07-24 17:41:29 +0530

    Ignore

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -8,21 +8,6 @@ public class Test
 {
     public static void main(String[] args)
     {
-        List<Integer> al = new ArrayList<Integer>();
-        al.add(10);
-        al.add(20);
-        al.add(30);
-        al.add(1);
-        al.add(1);
-        al.add(2);
 
-        // This makes a call to remove(int) and
-        // removes element 20.
-        al.remove(1);
-
-        // Now element 30 is moved one position back
-        // So element 30 is removed this time
-
-        System.out.println("Modified ArrayList : " + al);
     }
 }
